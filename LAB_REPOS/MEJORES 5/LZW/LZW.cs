2b6da9e17b59dc9e24e94362fc559c8218da534d
Gerardo0112 <gerar0112@gmail.com>
2020-04-30 09:31:12 -0600

    Se inicia proceso del llenado del diccionario.

--- LAB_REPOS/MEJORES 5/LZW/LZW.cs
+++ LAB_REPOS/MEJORES 5/LZW/LZW.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,17 @@ namespace LAB_REPOS.MEJORES_5.LZW
     {
         private Dictionary<string, int> compress = new Dictionary<string, int>();
         public double bytes_compression, bytes_original;
+        //Llenar diccionario.
+        public void dictionary_initial(FileStream file)
+        {
+            //var split = Nombre.Split('.');
+            // bytes_original = file.ContentLength;
+            List<int> initial_list = new List<int>();
+            Dictionary<string, int> Base = new Dictionary<string, int>();
+            var length = 0;
+            //Leer archivo.
+            //Stream stream = file.InputStream;
+            length = 1000;
+        }
     }
 }
