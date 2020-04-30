09659e4708cea6b80b29343b936ab0eb7c5492ef
aleve <alevelasquezmorales@gmail.com>
2020-04-29 18:31:03 -0600

    Añadir clases en folder CIFRADOS

---
+++ LAB_REPOS/MEJORES 5/CIFRADOS/Spiral_Encryption.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB_REPOS.MEJORES_5.CIFRADOS
+{
+    public class Spiral_Encryption
+    {
+    }
+}
