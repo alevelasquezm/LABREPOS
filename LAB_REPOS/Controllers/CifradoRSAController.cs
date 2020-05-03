95f0f1157ce161255bbce150f9b7cdb0056c3e6c
aleve <alevelasquezmorales@gmail.com>
2020-05-02 20:06:40 -0600

    cambios api RSA

--- LAB_REPOS/Controllers/CifradoRSAController.cs
+++ LAB_REPOS/Controllers/CifradoRSAController.cs
@@ -46,7 +46,7 @@ namespace LAB_REPOS.Controllers
         }
 
         // POST api/RSA
-        [HttpGet("getPublicKey")]
+        [Route("getPublicKey")]
         public void GenerarLlaves([FromBody] Keys RSA, string file)
         {
             var primo1 = RSA.numeroPrimo(RSA.ValorP, 2);
